72c15695d073e615ac529f4d0cf5ff30c46488c8
andriell <root.andriell@gmail.com>
2017-02-04 11:55:11 +0300

    start

--- src/main/java/com/andriell/tesseract/gui/MainFrame.java
+++ src/main/java/com/andriell/tesseract/gui/MainFrame.java
@@ -3,6 +3,8 @@ package com.andriell.tesseract.gui;
 import com.andriell.tesseract.figure.Tesseract;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * Created by Андрей on 04.02.2017.
@@ -14,13 +16,22 @@ public class MainFrame {
     private JPanel rightPane;
     private JPanel topPane;
     private JSlider slider01;
-    private JButton repaintButton;
-    private JButton saveButton;
     private JSlider slider02;
     private JSlider slider12;
     private JSlider slider30;
     private JSlider slider31;
     private JSlider slider32;
+    private JButton repaintButton;
+    private JButton saveButton;
+    private JCheckBox autoCheckBox;
+    private JLabel value01;
+    private JLabel value02;
+    private JLabel value12;
+    private JLabel value30;
+    private JLabel value32;
+    private JLabel value31;
+
+    Tesseract tesseract;
 
     public MainFrame() {
 
@@ -28,24 +39,60 @@ public class MainFrame {
 
     public void show() {
         frame = new JFrame("Gui");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setSize(740, 460);
-
-        frame.setContentPane(rootPanel);
-
-        Tesseract tesseract;
         tesseract = new Tesseract(100);
-        tesseract.rotation(0, 1, 45);
-        tesseract.rotation(1, 2, 45);
-        tesseract.rotation(2, 3, 45);
-        tesseract.rotation(3, 0, 45);
-        tesseract.rotation(3, 1, 45);
-        tesseract.rotation(3, 2, 45);
-        tesseract.repaint();
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(740, 460);
+        frame.setContentPane(rootPanel);
         centerPane.add(new ImagePane(tesseract.getImage()));
 
+        ChangeListener changeListener = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (autoCheckBox.isEnabled()) {
+                    update();
+                }
+            }
+        };
+        slider01.addChangeListener(changeListener);
+        slider02.addChangeListener(changeListener);
+        slider12.addChangeListener(changeListener);
+        slider30.addChangeListener(changeListener);
+        slider31.addChangeListener(changeListener);
+        slider32.addChangeListener(changeListener);
+
+        update();
+
         frame.setVisible(true);
+    }
+
+    private void update() {
+        int value;
+        value = slider01.getValue();
+        value01.setText(Integer.toString(value));
+        tesseract.rotation(0, 1, value);
+
+        value = slider02.getValue();
+        value02.setText(Integer.toString(value));
+        tesseract.rotation(0, 2, value);
+
+        value = slider12.getValue();
+        value12.setText(Integer.toString(value));
+        tesseract.rotation(1, 2, value);
+
+        value = slider30.getValue();
+        value30.setText(Integer.toString(value));
+        tesseract.rotation(3, 0, value);
+
+        value = slider31.getValue();
+        value31.setText(Integer.toString(value));
+        tesseract.rotation(3, 1, value);
+
+        value = slider32.getValue();
+        value32.setText(Integer.toString(value));
+        tesseract.rotation(3, 2, value);
+
+        tesseract.repaint();
+        centerPane.repaint();
     }
 }
